6ed885dce37c458a812bca0f8b8d9de187f2ab2f
Anandagadugin <gaduginanand984@gmail.com>
2023-03-26 20:30:06 +0530

    Bootstrap 5 and some Js operations

---
+++ main.js
@@ -0,0 +1,42 @@
+//Concatination
+// let age = 30;
+// let name = "anand";
+// console.log('My name is ' + name + ' I am ' + age + ' years old');
+
+// //temlate string
+// const details = `My name is ${name} I am ${age} years old`;
+// console.log(details);
+
+//spliting Ex1
+// const name = 'Hello Anand';
+// console.log(a.split(''));
+// Ex2
+// const name = 'Anand G' , age = 21 , address = 
+// "Prestige Falcon City, Bangalore";
+
+// console.log("personal details = My name is ",name, "age is", age," address is", address)
+
+// //reverse
+// let name = 'ram';
+// console.log(name.split('').reverse().join(''));
+
+// const arr = [4,5,3,5,6,8];
+// const min = Math.max(...arr);
+// console.log(min);
+
+//Object literals
+const person = {
+    firstName: 'virat',
+    lastName: 'kohli',
+    age: 30,
+    hobbies:['cricket', 'advertisment', 'sports'],
+    address: { 
+        street:'Shabbir nagar',
+        city:'bangalore',
+        state:'Karnataka'
+    }
+}
+console.log(person.firstName, person.lastName);
+console.log(person.hobbies[0]);
+console.log(person.address.state);
+
